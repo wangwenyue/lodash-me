d8da2663bfd6d4c1442e0c8704d9e4ef21ce628f
Wenyue Wang <realwwy@cryptape.com>
2018-12-12 22:40:08 +0800

    modify parseUrl.js

--- example/function/parseUrl.js
+++ example/function/parseUrl.js
@@ -9,48 +9,33 @@ const ensure = (condition, message) => {
 }
 
 /*
-  url 是字符串, 可能的值如下
-  'g.cn'
-  'g.cn/'
-  'g.cn:3000'
-  'g.cn:3000/search'
-  'http://g.cn'
-  'https://g.cn'
-  'http://g.cn/'
+  url 是字符串, 返回协议
 
   如果 url 中省略了协议部分, 那么默认用的是 http 协议
   如果没有省略, 则按照 url 中协议部分的解析
   返回代表协议的字符串, 'http' 或者 'https'
 */
+const urls = [
+  ['g.cn', 'http', 'g.cn', 80, '/'],
+  ['g.cn/', 'http', 'g.cn', 80, '/'],
+  ['g.cn:3000', 'http', 'g.cn', 3000, '/'],
+  ['g.cn:3000/search', 'http', 'g.cn', 3000, '/search'],
+  ['http://g.cn', 'http', 'g.cn', 80, '/'],
+  ['https://g.cn', 'https', 'g.cn', 443, '/'],
+  ['http://g.cn/', 'http', 'g.cn', 80, '/'],
+]
 
 const protocolOfUrl = url => url.startsWith('https://') ? 'https' : 'http'
 
 const testProtocolOfUrl = () => {
-  const urls = [
-    ['g.cn', 'http'],
-    ['g.cn/', 'http'],
-    ['g.cn:3000', 'http'],
-    ['g.cn:3000/search', 'http'],
-    ['http://g.cn', 'http'],
-    ['https://g.cn', 'https'],
-    ['http://g.cn/', 'http'],
-  ]
-
   for (let u of urls) {
-    const [url, protocol] = u
+    const [url, protocol, , , ,] = u
     ensure(protocolOfUrl(url) === protocol, 'test protocol of url')
   }
 }
 
 /*
-  url 是字符串, 可能的值如下
-  'g.cn'
-  'g.cn/'
-  'g.cn:3000'
-  'g.cn:3000/search'
-  'http://g.cn'
-  'https://g.cn'
-  'http://g.cn/'
+  url 是字符串, 返回主机
 
   返回代表主机的字符串, 比如 'g.cn'
 */
@@ -73,30 +58,14 @@ const hostOfUrl = url => {
 }
 
 const testHostOfUrl = () => {
-  const urls = [
-    ['g.cn', 'g.cn'],
-    ['g.cn/', 'g.cn'],
-    ['g.cn:3000', 'g.cn'],
-    ['g.cn:3000/search', 'g.cn'],
-    ['http://g.cn', 'g.cn'],
-    ['https://g.cn', 'g.cn'],
-    ['http://g.cn/', 'g.cn'],
-  ]
   for (let u of urls) {
-    const [url, host] = u
+    const [url, , host, , ,] = u
     ensure(hostOfUrl(url) === host, 'test host of url')
   }
 }
 
 /*
-  url 是字符串, 可能的值如下
-  'g.cn'
-  'g.cn/'
-  'g.cn:3000'
-  'g.cn:3000/search'
-  'http://g.cn'
-  'https://g.cn'
-  'http://g.cn/'
+  url 是字符串, 返回端口
 
   返回代表端口的数字, 比如 80 或者 3000
   80 是 http 协议的默认端口, 443 是 https 协议的默认端口
@@ -129,30 +98,14 @@ const portOfUrl = url => {
 }
 
 const testPortOfUrl = () => {
-  const urls = [
-    ['g.cn', 80],
-    ['g.cn/', 80],
-    ['g.cn:3000', 3000],
-    ['g.cn:3000/search', 3000],
-    ['http://g.cn', 80],
-    ['https://g.cn', 443],
-    ['http://g.cn/', 80],
-  ]
   for (let u of urls) {
-    const [url, port] = u
+    const [url, , , port, ,] = u
     ensure(portOfUrl(url) === port, 'test port of url')
   }
 }
 
 /*
-  url 是字符串, 可能的值如下
-  'g.cn'
-  'g.cn/'
-  'g.cn:3000'
-  'g.cn:3000/search'
-  'http://g.cn'
-  'https://g.cn'
-  'http://g.cn/'
+  url 是字符串, 返回路径
 
   返回代表路径的字符串, 比如 '/' 或者 '/search'
   当没有给出路径的时候, 默认路径是 '/'
@@ -174,30 +127,14 @@ const pathOfUrl = url => {
 }
 
 const testPathOfUrl = () => {
-  const urls = [
-    ['g.cn', '/'],
-    ['g.cn/', '/'],
-    ['g.cn:3000', '/'],
-    ['g.cn:3000/search', '/search'],
-    ['http://g.cn', '/'],
-    ['https://g.cn', '/'],
-    ['http://g.cn/', '/'],
-  ]
   for (let u of urls) {
-    const [url, path] = u
+    const [url, , , , path,] = u
     ensure(pathOfUrl(url) === path, 'test path of url')
   }
 }
 
 /*
-  url 是字符串, 可能的值如下
-  'g.cn'
-  'g.cn/'
-  'g.cn:3000'
-  'g.cn:3000/search'
-  'http://g.cn'
-  'https://g.cn'
-  'http://g.cn/'
+  url 是字符串, 返回解析后的 url
 
   返回一个 object, 内容如下
   {
@@ -218,15 +155,6 @@ const parsedUrl = url => {
 }
 
 const testParsedUrl = () => {
-  const urls = [
-    ['g.cn', 'http', 'g.cn', 80, '/'],
-    ['g.cn/', 'http', 'g.cn', 80, '/'],
-    ['g.cn:3000', 'http', 'g.cn', 3000, '/'],
-    ['g.cn:3000/search', 'http', 'g.cn', 3000, '/search'],
-    ['http://g.cn', 'http', 'g.cn', 80, '/'],
-    ['https://g.cn', 'https', 'g.cn', 443, '/'],
-    ['http://g.cn/', 'http', 'g.cn', 80, '/'],
-  ]
   for (let u of urls) {
     const [url, p1, h, p2, p3] = u
     const { protocol, host, port, path } = parsedUrl(url)
