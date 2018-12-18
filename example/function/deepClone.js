065f959e80d501ba8dd7bec280463a153961b875
wangwenyue <wangwenyue1993@gmail.com>
2018-12-18 11:54:10 +0800

    modify deep clone

--- example/function/deepClone.js
+++ example/function/deepClone.js
@@ -3,7 +3,7 @@ const log = console.log.bind(console, '### deepClone')
 const deepClone = obj => {
   // 先 shallow clone
   // let clone = Object.assign({}, obj)
-  let clone = {...obj}
+  let clone = { ...obj }
   // 检查每一个 key，若 value 是 obj, 则递归深拷贝，否则直接赋值
   Object.keys(clone).forEach(
     key => (clone[key] = typeof obj[key] === 'object' ? deepClone(obj[key]) : obj[key])
@@ -25,11 +25,24 @@ const deepClone = obj => {
 // 最简单的办法，不过有点缺陷，某些特殊情况不能用，比如值是 undefined
 const deepClone2 = obj => JSON.parse(JSON.stringify(obj))
 
+// ruanyifeng version http://www.ruanyifeng.com/blog/2010/05/object-oriented_javascript_inheritance_continued.html
+function deepCopy(p, c={}) {
+  for (var i in p) {
+    if (typeof p[i] === 'object') {
+      c[i] = (p[i].constructor === Array) ? [] : {}
+      deepCopy(p[i], c[i])
+    } else {
+      c[i] = p[i]
+    }
+  }
+  return c
+}
+
 const testDeepClone = () => {
   const a = { foo: 'bar', obj: { a: 1, b: 2 }, arr: [1, 3, 5, 7] }
-  const b = deepClone(a)
+  const b = deepCopy(a)
   b.foo = 'foo of b'
-  const c = deepClone2(a)
+  const c = deepCopy(a)
   c.arr[1] = 9
   log(a)
   log(b)
