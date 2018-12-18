75f1cc618e35579bd795879bca258baf06fe3530
wangwenyue <wangwenyue1993@gmail.com>
2018-12-18 10:17:57 +0800

    modify deep equal

--- example/function/deepEqual.js
+++ example/function/deepEqual.js
@@ -7,23 +7,26 @@ const ensureEqual = (a, b, message) => {
 const equals = (a, b) => {
   // 简单类型的判断
   if (a === b) return true
-  // 时间类型的判断
+  // 时间类型的判断, 转换成时间戳判断
   if (a instanceof Date && b instanceof Date) return a.getTime() === b.getTime()
   // 基本类型的判断
   if (!a || !b || (typeof a !== 'object' && typeof b != 'object')) return a === b
   // a, b 有一个为 null 或者 undefined，则为 false
   if (a === null || b === null || a === undefined || b === undefined) return false
   // 判断 object 相等
-  if (Object.keys(a).length !== Object.keys(b).length) return false
-  return keys.every(k => equals(a[k], b[k]))
+  const keysA = Object.keys(a)
+  const keysB = Object.keys(b)
+  if (keysA.length !== keysB.length) return false
+  return keysA.every(k => equals(a[k], b[k]))
 }
 
 const equals2 = (a, b) => JSON.stringify(a) === JSON.stringify(b)
 
 const test1 = () => {
   const obj = { a: [2, { e: 3 }], b: [4], c: 'foo', d: undefined, e: null }
-  ensureEqual(equals(obj, obj), true, 'test deep equal 1')
-  ensureEqual(equals2(obj, obj), true, 'test deep equal 2')
+  const obj2 = { a: [2, { e: 3 }], b: [4], c: 'foo', d: undefined, e: null }
+  ensureEqual(equal(obj, obj2), true, 'test deep equal 1')
+  ensureEqual(equal2(obj, obj2), true, 'test deep equal 2')
 }
 
 const test2 = () => {
