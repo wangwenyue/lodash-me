6b687497e388fae12245d81e37468ce780a880aa
wangwenyue <wangwenyue1993@gmail.com>
2018-12-22 17:04:31 +0800

    modify deepEqual.js

--- example/function/deepEqual.js
+++ example/function/deepEqual.js
@@ -5,42 +5,65 @@ const ensureEqual = (a, b, message) => {
 }
 
 const equals = (a, b) => {
-  // 简单类型的判断
-  if (a === b) return true
-  // 时间类型的判断, 转换成时间戳判断
-  if (a instanceof Date && b instanceof Date) return a.getTime() === b.getTime()
-  // 基本类型的判断
-  if (!a || !b || (typeof a !== 'object' && typeof b != 'object')) return a === b
-  // a, b 有一个为 null 或者 undefined，则为 false
-  if (a === null || b === null || a === undefined || b === undefined) return false
-  // 判断 object 相等
-  const keysA = Object.keys(a)
-  const keysB = Object.keys(b)
-  if (keysA.length !== keysB.length) return false
-  return keysA.every(k => equals(a[k], b[k]))
+  // 原始类型的判断
+  if (a === b) {
+    return true
+  }
+  // object 的判断
+  if (a && b && typeof a === 'object' && typeof b === 'object' ) {
+    // array 的判断
+    if (Array.isArray(a) && Array.isArray(b)) {
+      const len = a.length
+      if (len !== b.length) return false
+      for (let i = 0; i <= len; i++) {
+        if (!equals(a[i], b[i])) {
+          return false
+        }
+      }
+      return true
+    }
+
+    if (a instanceof RegExp && b instanceof RegExp) {
+      return a.toString() === b.toString()
+    }
+
+    if (a instanceof Date && b instanceof Date) {
+      return a.getTime() === b.getTime()
+    }
+
+    const keys = Object.keys(a)
+    len = keys.length
+    if (len !== Object.keys(b).length) {
+      return false
+    }
+    // 是否属性都存在
+    for (let i = 0; i < len; i++)
+      if (!Object.prototype.hasOwnProperty.call(b, keys[i])) {
+        return false
+      }
+    // 是否属性的值都相等
+    for (let i = 0; i < len; i++) {
+      if (!equals(a[keys[i]], b[keys[i]])) {
+        return false
+      }
+    }
+    return true
+  }
 }
 
-const equals2 = (a, b) => JSON.stringify(a) === JSON.stringify(b)
+const testEquals = () => {
+  const obj = { a: [2, { e: 3 }], b: [4], c: 'foo', d: undefined, e: null, f: '/\d/g' }
+  const obj2 = { b: [4], c: 'foo', d: undefined, e: null, a: [2, { e: 3 }], f: '/\d/g' }
+  const obj3 = { b: [4, 5], c: 'foo', d: undefined, e: null, a: [2, { e: 3 }] }
+  const obj4 = { b: [4, 5], c: 'foo', d: undefined, e: null }
 
-const test1 = () => {
-  const obj = { a: [2, { e: 3 }], b: [4], c: 'foo', d: undefined, e: null }
-  const obj2 = { a: [2, { e: 3 }], b: [4], c: 'foo', d: undefined, e: null }
-  ensureEqual(equals(obj, obj2), true, 'test deep equal 1')
-  ensureEqual(equals2(obj, obj2), true, 'test deep equal 2')
-}
-
-const test2 = () => {
-  // 若调换 obj 中属性的位置，则 equals 报错， equals2 不相等
-  const obj = { a: [2, { e: 3 }], b: [4], c: 'foo', d: undefined, e: null }
-  const obj2 = { b: [4], c: 'foo', d: undefined, e: null, a: [2, { e: 3 }] }
-  ensureEqual(equals(obj, obj2), true, 'test deep equal 1') // 报错
-  ensureEqual(equals2(obj, obj2), true, 'test deep equal 2') // 不相等
-
+  ensureEqual(equals(obj, obj2), true, 'test deep obj equal 1')
+  ensureEqual(equals(obj, obj3), false, 'test deep obj equal 2')
+  ensureEqual(equals(obj, obj4), false, 'test deep obj equal 3')
 }
 
 const __main = () => {
-  test1()
-  // test2()
+  testEquals()
 }
 
 __main()
