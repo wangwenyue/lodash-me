2147db8b06e86afb8b02b927e29ea9fe51cf3658
wangwenyue <wangwenyue1993@gmail.com>
2018-12-22 21:14:41 +0800

    modify deepEquals.js

--- example/function/deepEquals.js
+++ example/function/deepEquals.js
@@ -5,7 +5,7 @@ const ensureEqual = (a, b, message) => {
 }
 
 // 高级版
-const equals = (a, b) => {
+const deepEquals = (a, b) => {
   if (a === b) return true
 
   if (a && b && typeof a === 'object' && typeof b === 'object' ) {
@@ -28,7 +28,7 @@ const equals = (a, b) => {
 }
 
 // 平民版
-const equals2 = (a, b) => {
+const deepEquals2 = (a, b) => {
   if (a === b) return true
   // 引用类型 判断
   if (a && b && typeof a === 'object' && typeof b === 'object') {
@@ -62,7 +62,7 @@ const equals2 = (a, b) => {
   }
 }
 
-const testEquals = () => {
+const testDeepEquals = () => {
   const obj = { a: [2, { e: 3 }], b: [4], c: 'foo', d: undefined, e: null, f: '/\d/g' }
   const obj2 = { b: [4], c: 'foo', d: undefined, e: null, a: [2, { e: 3 }], f: '/\d/g' }
   const obj3 = { b: [4, 5], c: 'foo', d: undefined, e: null, a: [2, { e: 3 }] }
@@ -70,19 +70,19 @@ const testEquals = () => {
   const obj5 = { a: 3, b: 4, d: 1}
   const obj6 = { b: 4, a: 3, d: 1}
 
-  ensureEqual(equals(obj, obj2), true, 'test deep obj equal 1')
-  ensureEqual(equals(obj, obj3), false, 'test deep obj equal 2')
-  ensureEqual(equals(obj, obj4), false, 'test deep obj equal 3')
-  ensureEqual(equals(obj5, obj6), true, 'test deep obj equal 4')
+  ensureEqual(deepEquals(obj, obj2), true, 'test deep obj equal 1')
+  ensureEqual(deepEquals(obj, obj3), false, 'test deep obj equal 2')
+  ensureEqual(deepEquals(obj, obj4), false, 'test deep obj equal 3')
+  ensureEqual(deepEquals(obj5, obj6), true, 'test deep obj equal 4')
 
-  ensureEqual(equals2(obj, obj2), true, 'test deep obj equal 1')
-  ensureEqual(equals2(obj, obj3), false, 'test deep obj equal 2')
-  ensureEqual(equals2(obj, obj4), false, 'test deep obj equal 3')
-  ensureEqual(equals2(obj5, obj6), true, 'test deep obj equal 4')
+  ensureEqual(deepEquals2(obj, obj2), true, 'test deep obj equal 1')
+  ensureEqual(deepEquals2(obj, obj3), false, 'test deep obj equal 2')
+  ensureEqual(deepEquals2(obj, obj4), false, 'test deep obj equal 3')
+  ensureEqual(deepEquals2(obj5, obj6), true, 'test deep obj equal 4')
 }
 
 const __main = () => {
-  testEquals()
+  testDeepEquals()
 }
 
 __main()
